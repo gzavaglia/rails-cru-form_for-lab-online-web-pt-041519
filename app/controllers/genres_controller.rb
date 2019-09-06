5b7173315e95b1c8d7e6671ab2407e33cc45b73f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-06 19:22:52 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,7 +8,7 @@ class GenresController < ApplicationController
   end
   
   def create
-    @genre = Genre.new(post_params)
+    @genre = Genre.new(genre_params)
     @genre.save
     redirect_to genre_path(@genre)
   end
@@ -19,14 +19,14 @@ class GenresController < ApplicationController
   
   def update
     @genre = Genre.find_by(params[:id])
-    @genre.update(post_params)
+    @genre.update(genre_params)
     redirect_to genre_path(@genre)
   end
   
   
   
-  
-  def post_params
+  private
+  def genre_params
     params.require(:genre).permit(:name)
   end
 end
