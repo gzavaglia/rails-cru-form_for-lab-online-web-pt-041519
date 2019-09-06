ddb99f1ecc5b2514dc1888e08f4b1d81d492e17a
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-06 19:19:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find_by(params[:id])
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.new(post_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+  
+  def edit 
+    @song = Song.find_by(params[:id])
+  end
+  
+  def update
+    @song = Song.find_by(params[:id])
+    @song.update(post_params)
+    redirect_to song_path(@song)
+  end
+  
+  
+  
+  
+  def post_params
+    params.require(:song).permit(:name)
+  end
 end
