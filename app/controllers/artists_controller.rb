fa39b1e18178433b3cc358370116495e4ade2eaa
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-06 19:09:40 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,6 +13,15 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
   
+  def edit 
+    @artist = Artist.find_by(params[:id])
+  end
+  
+  def update
+    @artist = Artist.find_by(params[:id])
+    @artist.update(post_params)
+    redirect_to artist_path(@artist)
+  end
   
   
   def post_params
