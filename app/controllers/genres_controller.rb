f91bfc1e7e7dcd0af6daba32fdd05bc1ef96b267
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-06 19:15:20 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,7 +4,19 @@ class GenresController < ApplicationController
   end
   
   def new
-    @genre = Genre.new(post_params)
+    @gen
   end
   
+  def create
+    @genre = Genre.new(post_params).
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+  
+  
+  
+  
+  def post_params
+    params.require(:genre).permit(:name)
+  end
 end
